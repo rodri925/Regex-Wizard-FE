012cfb03cf635a301d0206923b7c423491e5957f
rodri925 <malik.rodriguez@gmail.com>
2022-01-21 13:26:01 -0600

    Landing Page part 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import LandingPage from './components/LandingPage';
 
 const App = () => (
   <div className="App">
     <Navbar />
+    <LandingPage />
   </div>
 );
 
